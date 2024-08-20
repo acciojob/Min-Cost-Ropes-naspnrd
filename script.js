59bf5cff295b6056ca664c36ac52fbc739bdd05c
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-08-20 20:52:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) { 
+	arr.sort((a, b) => a - b);
+	let cost = 0;
+	while(arr.length >  1){
+		let first = arr.shift();
+		let second = arr.shift();
+		cost += first + second;
+		arr.push(first + second);
+		arr.sort((a, b) => a - b);
+	}
+	return cost;
 }
 
 module.exports=mincost;
